f6b5b71f807fd758617caa73fd0d8042c57fd650
cosmos <sensewan@gmail.com>
2021-05-06 14:54:59 +0900

    상품 페이지 대략적인 기능1

---
+++ src/main/java/com/project/dao/ProductDao.java
@@ -0,0 +1,43 @@
+package com.project.dao;
+
+import java.util.List;
+
+import com.project.domain.Product;
+
+public interface ProductDao {
+	
+	/* 한 페이지에 보여 질 게시글 리스트와 검색 리스트 요청 시 호출되는 메소드
+	 * 현재 페이지에 해당하는 게시글 리스트를 DB에서 읽어와 반환 
+	 **/
+	public abstract List<Product> productList(int startRow, int num, String type, String keyword);
+	
+	
+	
+	
+	/* 게시 글 수를 계산하기 위해 호출되는 메서드 - paging 처리에 사용
+	 * 게시 글 리스트와 검색 리스트에 대한 게시 글 수를 반환 하는 메서드
+	 **/
+	public abstract int getProductCount(String type, String keyword);
+	
+	
+	
+	/* 게시 글 상세보기 요청 시 호출되는 메서드
+	 * no에 해당하는 게시 글 을 DB에서 읽어와 Board 객체로 반환 하는 메서드 
+	 * isCount == true 면 게시 글 읽은 횟수를 1 증가시킨다.
+	 **/
+	public abstract Product getProduct(int no, boolean isCount);
+	
+	
+	
+	/* 게시 글쓰기 요청 시 호출되는 메서드
+	 * 게시 글쓰기 요청 시 게시 글 내용을 Product 객체로 받아 DB에 추가하는 메서드 
+	 **/
+	public abstract void insertProduct(Product product);
+	
+	
+	// 상품 수정하기
+	public abstract void updateProduct(Product product);
+	
+	
+
+}
--- src/main/webapp/WEB-INF/views/pages/header.jsp
+++ src/main/webapp/WEB-INF/views/pages/header.jsp
@@ -38,7 +38,7 @@
 		        <a class="nav-link" href="">건강정보</a>
 		      </li>
 		      <li class="nav-item">
-		        <a class="nav-link" href="">비타민</a>
+		        <a class="nav-link" href="productList">비타민</a>
 		      </li>
 		      <li class="nav-item">
 		        <a class="nav-link" href="">커뮤니티</a>
